029d69f7b84a22a5ceb3cfe975827df3a3ef640d
ADV <a.deverdilhac@gmail.com>
2020-12-09 12:24:33 +0100

    [ade-verd] init

---
+++ scripts/startVM.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+NAME="boot"
+
+CURDIR=`dirname $0`
+
+# set -x
+VM_NAME=`VBoxManage list vms | grep -i $NAME | head -1 | grep -oP '\"\K[^" ]+'`
+if [ -z "$VM_NAME" ]; then
+    echo "The VM $NAME does not exist. Please install it first"
+    exit 1;
+fi
+
+IS_VM_RUNNING=`vboxmanage showvminfo "$VM_NAME" | grep -c "running (since"`
+if [ "$IS_VM_RUNNING" -eq "0" ]; then
+    VBoxManage startvm $VM_NAME --type headless
+else
+    echo "VM $VM_NAME is already running"
+fi
+
+echo "Waiting for the IP..."
+n=0
+until [ "$n" -ge 60 ]
+do
+    MAC_ADDRESS=`VBoxManage showvminfo $VM_NAME | grep -oP 'MAC: \K[^",]+' | sed -e 's/[0-9A-F]\{2\}/&:/g' -e 's/:$//'`
+    VM_IP=`ip neighbor | grep -i $MAC_ADDRESS | cut -d' ' -f1`
+    # VM_IP=`VBoxManage guestproperty enumerate $VM_NAME | grep -i ip | grep -ioP 'value: \K[^,]+'`
+    if [ -n "$VM_IP" ]; then break; fi
+    n=$((n+1)) 
+    sleep 1
+done
+if [ -z "$VM_IP" ]; then
+    echo -e "Failed to get the IP. Please run the following command and try again\n"
+    echo "sudo service virtualbox restart"
+    exit 1;
+fi
+
+echo -e "You should set its IP as environment variable\n"
+echo "export B2R_HOST=$VM_IP"
+echo "export B2R_PORT=$VM_PORT"
---
+++ writeup1.md
@@ -0,0 +1,47 @@
+# Root privilege escalation - Method 1
+
+## Get VM IP
+
+```shell
+# On host machine
+VM_NAME="boot2Root"
+
+MAC_ADDRESS=`VBoxManage showvminfo $VM_NAME | grep -oP 'MAC: \K[^",]+' | sed -e 's/[0-9A-F]\{2\}/&:/g' -e 's/:$//'`
+ip neighbor | grep -i $MAC_ADDRESS | cut -d' ' -f1
+
+  192.168.1.72
+```
+
+We could export this value an environment variable: `export B2R_HOST=192.168.1.72`
+
+---
+
+## Scan Ports
+
+Requirements: nmap
+
+```shell
+nmap $B2R_HOST
+
+  Starting Nmap 7.60 ( https://nmap.org ) at 2020-12-09 11:20 CET
+  Nmap scan report for BornToSecHackMe-1.home (192.168.1.72)
+  Host is up (0.00016s latency).
+  Not shown: 994 closed ports
+  PORT    STATE SERVICE
+  21/tcp  open  ftp
+  22/tcp  open  ssh
+  80/tcp  open  http
+  143/tcp open  imap
+  443/tcp open  https
+  993/tcp open  imaps
+
+  Nmap done: 1 IP address (1 host up) scanned in 0.06 seconds
+```
+
+---
+
+### Sources
+
+## Hack
+
+## Man
