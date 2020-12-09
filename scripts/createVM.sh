c604ff58c98fc23483b4282ecf9c569195990f47
ADV <a.deverdilhac@gmail.com>
2020-12-09 22:58:14 +0100

    [ade-verd] fix createVM.sh

--- scripts/createVM.sh
+++ scripts/createVM.sh
@@ -46,6 +46,7 @@ function installVM {
     #Set memory and network
     selectBridgedInterface
     VBoxManage modifyvm $MACHINENAME --nic1 bridged --bridgeadapter1 $BRIDGED_INTERFACE
+    VBoxManage modifyvm $MACHINENAME --cableconnected1 on
     VBoxManage modifyvm $MACHINENAME --memory 1024 --vram 128
     
     #Create Disk and connect Iso
@@ -55,6 +56,7 @@ function installVM {
     VBoxManage storagectl $MACHINENAME --name "IDE Controller" --add ide --controller PIIX4
     VBoxManage storageattach $MACHINENAME --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $ISOPATH
     VBoxManage modifyvm $MACHINENAME --boot1 dvd --boot2 disk --boot3 none --boot4 none
+    VBoxManage modifyvm $MACHINENAME --graphicscontroller vmsvga
 }
 
 function selectBridgedInterface {
